042250e8bdc25d4401f6697b167fb87ee4d1042d
tanicha <tanijcha@gmail.com>
2023-02-27 13:07:08 -0800

    modified bag class and updated test

--- Bag.js
+++ Bag.js
@@ -1,10 +1,16 @@
 class Bag {
+    static maxWeight = 23; 
     constructor(weight) {
         if(!weight){
             throw new Error('bag needs weight')
-        } 
-        this.weight = weight
+        } else if (weight > Bag.maxWeight){
+            throw new Error ('bag is over the limit of 23kg')
+        } else {
+            this.weight = weight
+        }
     }
 }
+
+// console.log(Bag.weight);
 
 module.exports = Bag
--- Bag.test.js
+++ Bag.test.js
@@ -1,13 +1,16 @@
 const Bag = require('./Bag')
-
 describe('Bag class', () => {
     //bag has weight
     test('bag has weight', () => {
-        const bag = new Bag(25)
-        expect(bag.weight).toBe(25)
+        const bag = new Bag(21)
+        expect(bag.weight).toBe(21)
     })
     //if bag has no weight, return error
     test('bag has no weight to return an error', () => {
         expect(() => new Bag()).toThrowError('bag needs weight')
+    })
+    //returns error if bag is too heavy
+    test('bag is over max weight', () => {
+        expect(() => new Bag(25)).toThrowError('bag is over the limit of 23kg');
     })
 })
